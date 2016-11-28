535fc63486efe260136fe32ff8c20336c5a1bc3b
Toyraple <toyraple8@gmail.com>
2016-11-28 13:43:08 +0200

    UI tweaks
    Card drawing works correctly now (you can't overdraw).

--- HexagonGrid/Assets/Scripts/Card_Manager.cs
+++ HexagonGrid/Assets/Scripts/Card_Manager.cs
@@ -7,13 +7,13 @@ public class Card_Manager : MonoBehaviour {
         Hp = 0,
         Att = 0,
         Mov = 0,
-        Pow = 0;
+        Pow = 0,
+        highlighted = 0;
 
     public string Cardname;
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
--- HexagonGrid/Assets/Scripts/Game_Manager.cs
+++ HexagonGrid/Assets/Scripts/Game_Manager.cs
@@ -16,7 +16,9 @@ public class Game_Manager : MonoBehaviour {
 
     public List<GameObject> 
         DeckList1,
-        DeckList2;
+        DeckList2,
+        HandList1,
+        HandList2;
 
     GameObject Card_go;
 
@@ -24,6 +26,8 @@ public class Game_Manager : MonoBehaviour {
     void Start () {
         DeckList1 = new List<GameObject>();
         DeckList2 = new List<GameObject>();
+        HandList1 = new List<GameObject>();
+        HandList2 = new List<GameObject>();
 
 
         for (int i = 0; i < 5; i++)
@@ -63,41 +67,43 @@ public class Game_Manager : MonoBehaviour {
         switch (x)
         {
             case 1:
-
-                //if (PlayerHand1.transform.childCount < 6)
-                //{
                 if (DeckList1.Count > 0)
                 {
                     int rand1 = Random.Range(0, DeckList1.Count);
                     Card_go = (GameObject)Instantiate(DeckList1[rand1]);
-                    Card_go.transform.SetParent(PlayerHand1.transform);
-                    DeckList1.RemoveAt(rand1);
-                }
-                //}
 
-                /*else
-                {
-                    Discard();
-                }*/
+                    if (HandList1.Count < 5)
+                    {
+                        Card_go.transform.SetParent(PlayerHand1.transform);
+                        DeckList1.RemoveAt(rand1);
+                        HandList1.Add(Card_go);
+                    }
+                    else
+                    {
+                        Discard();
+                    }
+                }
                 break;
 
             case 2:
-                //if (PlayerHand2.transform.childCount > 6)
-                //{
                 if (DeckList2.Count > 0)
                 {
                     int rand2 = Random.Range(0, DeckList2.Count);
                     Card_go = (GameObject)Instantiate(DeckList1[rand2]);
-                    Card_go.transform.SetParent(PlayerHand2.transform);
-                    DeckList2.RemoveAt(rand2);
-                }
-                //}
 
-                /*else
-                {
-                    Discard();
-                }*/
+                    if (HandList1.Count < 5)
+                    {
+                        Card_go.transform.SetParent(PlayerHand2.transform);
+                        DeckList2.RemoveAt(rand2);
+                        HandList2.Add(Card_go);
+                    }
+                    else
+                    {
+                        Discard();
+                    }
+                }
                 break;
+
         }
     }
 
